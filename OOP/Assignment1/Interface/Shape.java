28f0826427ad5fb9796bab1701bcef7da307af61
meta-pulkit-saxena <pulkit.saxena@metacube.com>
2015-08-25 17:29:23 +0530

    Update Shape.java

--- OOP/Assignment1/Interface/Shape.java
+++ OOP/Assignment1/Interface/Shape.java
@@ -1,14 +1,14 @@
-package Assignment1;
-
 /**
- * Shape
+ * @class  Shape
  * @author Pulkit
- * contains signature of functons of shape
+ * contains signature of functions of shape to be extended by other shapes class.
  */
+
+package Assignment1;
 
 public interface Shape 
 {
-	 public void draw();
-	 public double getArea();
+	 public void draw();					// Declaration of the functions only.
+	 public double getArea();				// Declaration of function only.
 
 }
