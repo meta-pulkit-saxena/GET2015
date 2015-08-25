191a3967d93f8eb91ac70b8903ae2f936640dcaf
meta-pulkit-saxena <pulkit.saxena@metacube.com>
2015-08-25 17:30:40 +0530

    Update Square.java

--- OOP/Assignment1/Interface/Square.java
+++ OOP/Assignment1/Interface/Square.java
@@ -1,25 +1,24 @@
-package Assignment1;
-
 /**
- * Square
+ * @class Square
  * @author Pulkit
  * @since  12th August 15
  * Implements shape class for square
  */
+
+package Assignment1;
 public class Square implements Shape {
 	private int side;
 
 	/**
-	 * Square 
 	 * @param{side} side
-	 * Constructor of class Square
+	 * Constructor of class Square. It initializes the object with side argument very moment it is created.
 	 */
+	
 	public Square(int side) {
-		this.side = side;
+		this.side = side;														// Initializes the side member of the object.
 	}
 
 	/**
-	 * draw
 	 * Prints the square 
 	 */
 	
@@ -28,13 +27,12 @@ public class Square implements Shape {
 	}
 	
 	/**
-	 * getArea
 	 * @return{double} area
-	 * returns the area of square
+	 * returns the area of square of the object by which it is called.
 	 */
 
 	public double getArea() {
-		return (side * side);
+		return (side * side);													// Returns the area of the square.
 	}
 
 }
