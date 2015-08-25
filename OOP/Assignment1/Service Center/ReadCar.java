2921c35c18c516b14507d0a6407f69a2bb240626
meta-pulkit-saxena <pulkit.saxena@metacube.com>
2015-08-25 17:35:14 +0530

    Update ReadCar.java

--- OOP/Assignment1/Service Center/ReadCar.java
+++ OOP/Assignment1/Service Center/ReadCar.java
@@ -1,49 +1,54 @@
-package Assignment1;
+/**
+ * @class ReadCarFile
+ * @author Pulkit
+ * @since  13th August 15
+ * This class reads the car csv file and returns list of car type.
+ */
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * ReadCar
- * @author Pulkit
- * @since  13th August 15
- * Reads car details from csv file
- */
-
-public class ReadCar {
-
-	private List<String> cars = new ArrayList<String>();
+public class ReadCarFile {
+	private List<Car> carList = new ArrayList<Car>();
 	private String address = "D:/Car.csv";
 	private String breakPoint = ",";
 	
 	/**
-	 * readFile
-	 * @return{List<String}
-	 * Reads the data
+	 * This method is called when we need to read car details from csv file and store them in car type object.
+	 * @return{List<Car>}
 	 */
 	
-	public List<String> readFile() { 
-		ReadCar obj = new ReadCar();
+	public List<Car> readCarFile() {
+		
 		BufferedReader fileReader = null;
+		Car tempObj = null;
 		try {
 			String line = "";
-			fileReader  = new BufferedReader(new FileReader(obj.address));                           				// file reading
+			fileReader  = new BufferedReader(new FileReader(this.address));                           	// file reading
 			fileReader.readLine();
 			while ((line = fileReader.readLine()) != null) {
-				String[] tokens = line.split(obj.breakPoint);														// Separator
+				
+
+				String[] tokens = line.split(breakPoint);												// Separator
 				if (tokens.length > 0) {
-					obj.cars.add(tokens[0] + tokens[1]);															// Adding to the list
+						tempObj = new Car(2,"se");
+						tempObj.setCarNum(Integer.parseInt(tokens[0]));
+						tempObj.setCarType(tokens[1]);  												// Adding to the object
+																							
 				}
+				this.carList.add(tempObj);																// Adding the object to the list.
+				                                             
 			}
+			
 			fileReader.close();
 		} 
 		catch (Exception e) {
 			e.printStackTrace();
 		}
-		return obj.cars;
+		System.out.print(" q" + carList.size() + " q");
+		return carList;
 	}
+
 }
