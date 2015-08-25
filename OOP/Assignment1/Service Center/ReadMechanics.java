391a091e18e73e8f7d90b14e20c20ae880fca6e3
meta-pulkit-saxena <pulkit.saxena@metacube.com>
2015-08-25 17:36:03 +0530

    Update ReadMechanics.java

--- OOP/Assignment1/Service Center/ReadMechanics.java
+++ OOP/Assignment1/Service Center/ReadMechanics.java
@@ -1,45 +1,48 @@
-package Assignment1;
+/**
+ * @class  ReadMechanicFile
+ * @author Pulkit
+ * @since  13th August 15
+ * This class reads the mechanics csv file and returns the list of objects of mechanic class.
+ */
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * ReadMechanics
- * @author Pulkit
- * @since  13th August 15
- * Reads the mechanics details from file
- */
-
-public class ReadMechanics {
-	
+public class ReadMechanicFile {
 	private String address = "D:/Mechanic.csv";													// Address of file
-	private List<String> mechanic = new ArrayList();
-		
-	/**
-	 * readCsvFile
-	 * @return{List<String> list of string
-	 * @throws IOException
-	 * This method returns the name of person who is mechanic and his details
-	 **/
+	private String breakPoint = ",";															// separator
+	private List<Mechanic> mechanicList = new ArrayList<Mechanic>();
 	
-	public List<String> readCsvFile() {
-		ReadMechanics obj = new ReadMechanics();
+	/**
+	 * This reads the mechanic details from the file , stores it in list and returns
+	 * @return{List<Mechanic>}
+	 */
+	
+	public List<Mechanic> readMechanicFile() {
+		ReadMechanicFile obj = new ReadMechanicFile();
 		BufferedReader fileReader = null;
+		Mechanic tempObj = null;
 		try {
 			String line = "";
 			fileReader = new BufferedReader(new FileReader(obj.address));						//read from file;
 			fileReader.readLine();
-			while ((line = fileReader.readLine()) != null) {									
-					obj.mechanic.add(line);														// add it to file
+			while ((line = fileReader.readLine()) != null) {	
+				String[] tokens = line.split(breakPoint);	
+				if(tokens.length > 2) {
+					tempObj = new Mechanic("nam", "m",100);
+					tempObj.setName(tokens[0]);													// Adding name to the file
+					tempObj.setServiceType(tokens[1]);											//Adding service type to the file
+					tempObj.setCharge(Float.parseFloat(tokens[2]));
+				}
+				mechanicList.add(tempObj);
 			}
 			fileReader.close();																	// close the File
 		} 
 		catch (Exception e){
 				e.printStackTrace();
 		}
-		return obj.mechanic;
+		return mechanicList;
 	}
 }
-
