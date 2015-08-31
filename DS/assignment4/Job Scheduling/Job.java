33af423f5f8af3175980fd1048925e8e2a247b54
meta-pulkit-saxena <pulkit.saxena@metacube.com>
2015-08-31 19:39:39 +0530

    assignment4

---
+++ DS/assignment4/Job Scheduling/Job.java
@@ -0,0 +1,42 @@
+
+/**
+ * @class  Job
+ * @author Pulkit
+ * @since  30th August 15
+ * This class is a model class for job.
+ */
+
+package assignment4;
+
+public class Job {
+	private int priority;
+	private String document;
+	
+	/**
+	 * This is a parameterized constructor of job. 
+	 * @param{int} priority
+
+	 */
+	
+	public Job(int priority) {
+		this.priority = priority;
+	}
+	
+	/**
+	 * This function returns the priority of the job.
+	 * @return{int} priority
+	 */
+	
+	public int getPriority() {
+		return priority;
+	}
+	
+	/**
+	 * This function sets the priority of the job.
+	 * @param{int} priority
+	 */
+	
+	public void setPriority(int priority) {
+		this.priority = priority;
+	}
+}
---
+++ DS/assignment4/Room Booking/Hashing.java
@@ -0,0 +1,95 @@
+
+/**
+ * @class  Hashing
+ * @author Pulkit
+ * @since  30th August 15
+ * This class contains all the functions related to hashing.
+ */
+
+package assignment4;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class Hashing {
+	private Map<Integer,String> bookingStatus;
+	private int numberOfRooms;
+	private Guest guest;
+	
+	/**
+	 * This functin sets the guest.
+	 * @param{Guest} guest
+	 */
+	
+	public void setGuest(Guest guest) {
+		this.guest = guest;
+	}
+
+	/**
+	 * This is a constructor which sets the total number of rooms in the hotel.
+	 * @param{int} totalRooms
+	 */
+	
+	public Hashing(int totalRooms) {
+		this.numberOfRooms = totalRooms;
+		bookingStatus = new HashMap<Integer, String>();
+	}
+	
+	/**
+	 * This function calculates the hash value for each user on the basis of age.
+	 * if room corresponding to that hash value is available it is alloted.
+	 * @param{age} age
+	 * @return{int} 
+	 */
+	
+	public int hashValue(int age) {
+		int hashValue = age % this.numberOfRooms;
+		return hashValue;
+	}
+	
+	/**
+	 * This function allots the room to the guest.
+	 * @param{int} hashValue
+	 */
+	
+	public void allotRoom() {
+		int hashValue = hashValue(this.guest.getAge());
+		if(!bookingStatus.containsKey(hashValue)) {
+			bookingStatus.put(hashValue, this.guest.getName());
+		}
+		else {
+			avoidCollison(hashValue);
+		}
+	}
+	
+	
+	/**
+	 * This function in the situation of collision tries to find another room.
+	 * @param{int} hashValue
+	 */
+	
+	public void avoidCollison(int hashValue) {
+		int flag = 0;
+		for(int count = hashValue + 1;count <= numberOfRooms; count += 2) {
+			if(!bookingStatus.containsKey(count)) {
+				bookingStatus.put(count,this.guest.getName());
+				flag = 1;
+				break;
+			}
+		}
+		if(flag == 0) {
+			for(int count = 1;count <= numberOfRooms; count++) {
+				if(!bookingStatus.containsKey(count)) {
+					bookingStatus.put(count,this.guest.getName());
+				}
+			}
+		}
+	}
+	
+	public void displayBookingStatus() {
+		for(Map.Entry<Integer, String> entry : bookingStatus.entrySet()) {
+			System.out.println(entry.getKey() + " " + entry.getValue());
+		}
+	}
+}
