f5233aef7d1a592a0a2f560dd66ae420cca52b7d
meta-pulkit-saxena <pulkit.saxena@metacube.com>
2015-09-03 17:50:23 +0530

    Update CounselingSystem.java

--- DS/Assignment2/Queue and counseling System/CounselingSystem.java
+++ DS/Assignment2/Queue and counseling System/CounselingSystem.java
@@ -6,8 +6,6 @@
  * This class contains all the functions necessary to perform the counseling for college.
  */
 
-package RevisedAssignment2;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
