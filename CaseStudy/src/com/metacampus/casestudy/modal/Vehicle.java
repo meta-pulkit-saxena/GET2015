c86dbded39bae0ece50c6aab8fa0363542e86ca1
meta-pulkit-saxena <pulkit.saxena@metacube.com>
2015-09-18 16:50:43 +0530

    Update Vehicle.java

--- CaseStudy/src/com/metacampus/casestudy/modal/Vehicle.java
+++ CaseStudy/src/com/metacampus/casestudy/modal/Vehicle.java
@@ -1,3 +1,11 @@
+
+/**
+ * @class  Vehicle
+ * @author Pulkit
+ * @since  14th September 15
+ * This is a modal class for vehicle.
+ */
+
 package com.metacampus.casestudy.modal;
 
 public class Vehicle {
@@ -9,60 +17,163 @@ public class Vehicle {
 	private float price;
 	private float roadTax;
 	
+	/**
+	 * This is a default constructor.
+	 */
+	
 	public Vehicle() {
 	}
 	
-	public Vehicle(String modal, String make) {
-		this.model = modal;
+	/**
+	 * This is a parameterized constructor.
+	 * @param{String} model
+	 * @param{String} make
+	 */
+	
+	public Vehicle(String model, String make) {
+		this.model = model;
 		this.make = make;
 	}
+	
+	/**
+	 * This function calculates the on road price for the vehicle.
+	 * @return{float}
+	 */
 	
 	public float calculateOnRoadPrice() {
 		return (price + roadTax);
 	}
 	
+	/**
+	 * This function returns the make of the vehicle.
+	 * @return{String}
+	 */
+	
 	public String getMake() {
 		return make;
 	}
+	
+	/**
+	 * This function sets the make of the vehicle.
+	 * @param{String} make
+	 */
+	
 	public void setMake(String make) {
 		this.make = make;
 	}
+	
+	/**
+	 * This function returns the model of the vehicle.
+	 * @return{String }
+	 */
+	
 	public String getModel() {
 		return model;
 	}
+	
+	/**
+	 * This function sets model of the vehicle.
+	 * @param{String} model
+	 */
+	
 	public void setModel(String model) {
 		this.model = model;
 	}
+	
+	/**
+	 * This function returns the engine in cc.
+	 * @return{int}
+	 */
+	
 	public int getEngineInCC() {
 		return engineInCC;
 	}
+	
+	/**
+	 * This function sets the engine in cc.
+	 * @param{int} engineInCC
+	 */
+	
 	public void setEngineInCC(int engineInCC) {
 		this.engineInCC = engineInCC;
 	}
+	
+	/**
+	 * This function returns the fuel capacity.
+	 * @return{int}
+	 */
+	
 	public int getFuelCapacity() {
 		return fuelCapacity;
 	}
+	
+	/**
+	 * This function sets the fuel capacity.
+	 * @param{int} fuelCapacity
+	 */
+	
 	public void setFuelCapacity(int fuelCapacity) {
 		this.fuelCapacity = fuelCapacity;
 	}
+	
+	/**
+	 * This function returns the mileage of the vehicle.
+	 * @return{int}
+	 */
+	
 	public int getMileage() {
 		return mileage;
 	}
+	
+	/**
+	 * This function sets the mileage of the vehicle.
+	 * @param{int} mileage
+	 */
+	
 	public void setMileage(int mileage) {
 		this.mileage = mileage;
 	}
+	
+	/**
+	 * This function returns the price of vehicle.
+	 * @return{float}
+	 */
+	
 	public float getPrice() {
 		return price;
 	}
+	
+	/**
+	 * This function sets the price of the vehicle.
+	 * @param{float} price
+	 */
+	
 	public void setPrice(float price) {
 		this.price = price;
 	}
+	
+	/**
+	 * This funtion returns the roadtax of the vehicle.
+	 * @return{float}
+	 */
+	
 	public float getRoadTax() {
 		return roadTax;
 	}
+	
+	/**
+	 * This function sets the road tax of the vehicle.
+	 * @param{float} roadTax
+	 */
+	
 	public void setRoadTax(float roadTax) {
 		this.roadTax = roadTax;
 	}
+	
+	/**
+	 * This function overrides the toString function for vehicle.
+	 * @overrides toString()
+	 */
 	
 	@Override
 	public String toString() {
